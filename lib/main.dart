7c7e7bad259791387f17afc653320ae30a3f2b47
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-25 09:38:58 +0900

    Pavlovaを表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp4());
+void main() => runApp(MyApp5());
+
+class MyApp5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pavlova Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Pavlova(),
+    );
+  }
+}
+
+class Pavlova extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                Text('COOK:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Text('FEEDS:'),
+                Text('4-6'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          Text('titleText'),
+          Text('subTitle'),
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final sample = Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 440,
+          child: leftColumn,
+        ),
+        Image.asset('images/karaage-100px.png'),
+      ],
+    );
+
+    return Scaffold(
+      body: Center(child: sample),
+    );
+  }
+}
 
 class MyApp4 extends StatelessWidget {
   @override
