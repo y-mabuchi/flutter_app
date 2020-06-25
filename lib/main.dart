6bef24c9f5108e04f2bcee0f1f71be8921a959ba
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-25 09:55:28 +0900

    画面遷移の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp5());
+void main() => runApp(MaterialApp(
+      title: 'Navigation',
+      home: FirstScreen(),
+    ));
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('1番目のルート'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('次の画面を開く'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('2番目のルート'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('戻る'),
+        ),
+      ),
+    );
+  }
+}
 
 class MyApp5 extends StatelessWidget {
   @override
